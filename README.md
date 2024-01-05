00aee42ccd34d54dbb13f6431e3896b608f189b8
Hazellie <ishazelpie@gmail.com>
2024-01-05 22:08:43 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ECRP Select Search
+
+This Chrome extension enhances the select dropdown functionality on the gov.eclipse-rp.net website by adding a search feature.
---
+++ content.js
@@ -0,0 +1,38 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const select = document.querySelector("select");
+
+  if (!select) return;
+
+  const br = document.createElement("br");
+  const selectParent = select.parentNode;
+  selectParent.insertBefore(br, select);
+
+  const searchInput = document.createElement("input");
+  searchInput.setAttribute("class", "inputbox");
+  searchInput.setAttribute(
+    "style",
+    `width: ${select.offsetWidth}px; margin-bottom: 1%`
+  );
+  searchInput.setAttribute("placeholder", "Search...");
+  selectParent.insertBefore(searchInput, br);
+
+  searchInput.addEventListener("input", () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    let firstMatchedOption = null;
+
+    for (const option of select.options) {
+      const optionText = option.text.toLowerCase();
+      const display = optionText.includes(searchTerm) ? "block" : "none";
+      option.style.display = display;
+      if (display === "block" && !firstMatchedOption) {
+        firstMatchedOption = option;
+      }
+    }
+
+    if (firstMatchedOption) {
+      select.value = firstMatchedOption.value;
+    }
+  });
+});
